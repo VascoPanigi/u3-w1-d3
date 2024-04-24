a88999bb0a2cdb75e8221becbe5d14e143cc8849
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-04-24 16:32:26 +0200

    adds sworking search bar

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import { Container, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Form, FormControl } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import fantasy from "../data/fantasy.json";
 
 const BookList = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredBooks, setBooks] = useState(fantasy);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.value.toLowerCase();
+    setSearchValue(searchValue);
+
+    const filteredBooks = fantasy.filter((book) => book.title.toLowerCase().includes(searchValue));
+    setBooks(filteredBooks);
+  };
+
   return (
     <Container>
+      <Form>
+        <FormControl type="text" placeholder="Search a book..." onChange={handleSearch} value={searchValue} />
+      </Form>
       <Row className="mt-5">
-        {fantasy.map((book) => (
+        {filteredBooks.map((book) => (
           <SingleBook key={book.asin} book={book} />
         ))}
       </Row>
